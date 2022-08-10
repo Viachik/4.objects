8845cd0de0bd3fb8a8c243558768f8236ba64ab2
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-08-11 00:14:52 +0300

    Change word to array

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,14 +4,13 @@ function solveEquation(a, b, c) {
   let D = b**2 - 4*a*c;
 
   if (D < 0) {
-    return "Нет решений";
+    return [];
+  } else if (D = 0) {
+    return [-b/2*a];
+  } else {
+    return [(-b + Math.sqrt(D))/2*a, (-b - Math.sqrt(D))/2*a];
   }
-	return D = 0 ? [-b/2*a] : [
-			(-b + Math.sqrt(D))/2*a,
-			(-b - Math.sqrt(D))/2*a
-		]
 }
-
 
 console.log(solveEquation(1, 4, 1))
 
