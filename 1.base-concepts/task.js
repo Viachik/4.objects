9401902d677c39d1c1727db636cb4f666fba439d
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-08-10 20:55:35 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,21 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  // Решить уравнение
+  let D = b**2 - 4*a*c;
+
+  if (D < 0) {
+    return "Нет решений";
+  }
+	return D = 0 ? [-b/2*a] : [
+			(-b + Math.sqrt(D))/2*a,
+			(-b - Math.sqrt(D))/2*a
+		]
 }
 
+
+console.log(solveEquation(1, 1, 1))
+
+/*
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
@@ -11,3 +23,4 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   return totalAmount;
 }
+*/
