5d0ab5cca966384979d4a332f567eb3d0736a1d8
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-08-17 21:44:24 +0300

    task 2 2Vlad

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -36,16 +36,16 @@ function worker(arr) {
 
 function makeWork(arrOfArr, func) {
   let max = 0;
-  let result = 0;
 
   for (let i = 0; i < arrOfArr.length; i++) {
+    let result = 0;
     result = func(arrOfArr[i]);
+
+    if (result > max) {
+      max = result;
+    }
   }
 
-  if (result > max) {
-    max = result;
-  }
-  
   return max;
 }
 
@@ -56,5 +56,35 @@ console.log(makeWork([[0, 0, 0], [-1, -100]], worker));
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = 0;
+  let max = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+      if (arr[i] < min) {
+      min = arr[i];
+    } 
+  }
+  return Math.abs(max - min)
 }
+
+function makeWork(arr, func) {
+  let max = 0;
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result = func(arr[i]);
+  }
+
+  if (result > max) {
+    max = result;
+  }
+  
+  return max;
+}
+
+console.log(makeWork([[10, 20, 30], [-40, -50, -65]], worker2));
+console.log(makeWork([[10, 10, 11], [20, 10]], worker2));
+console.log(makeWork([[0, 1, 2], [-1, -100]], worker2));
