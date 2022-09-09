92860a79456d060ad40e714f809635e2a3acdc97
Viacheslav Serokurov <slaveeekua@yandex.ru>
2022-09-09 23:59:21 +0300

    correct with Vlad

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,28 +10,59 @@ Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
+student1.setSubject("Algebra");
+student2.setSubject("Geometry");
+
 Student.prototype.addMark = function addMark(mark) {
   if(this.marks === undefined){ 
-    this.marks = marks;
+    this.marks = [mark];
   } else {
     this.marks.push(mark);
   }
 }
 
-Student.prototype.addMarks = function addMarks(...mark) {
-  this.marks.push(...mark);
+student1.addMark(5);
+student2.addMark(2);
+
+Student.prototype.addMarks = function (...mark) {
+  if(this.mark === undefined){ 
+    this.marks = [mark];
+  } else {
+    this.marks.push(...mark);
+  }
 }
 
-Student.prototype.getAverage = function getAverage(marks) {
-  return this.marks.reduce((acc, mark)=> acc + mark, 0)/marks.length;
+student1.addMark(4);
+student1.addMark(5);
+student2.addMark(3);
+student2.addMark(2);
+
+Student.prototype.getAverage = function (marks) {
+  const avarage = this.marks.reduce((acc, mark)=> acc + mark, 0)/this.marks.length;
+  return Math.round(avarage);
 }
 
-Student.prototype.exclude = function exclude(reason) {
-  if (student.hasOwnProperty('subject')) {
+console.log(student1.getAverage());
+console.log(student1);
+
+Student.prototype.exclude = function (reason) {
+  if (this.student.hasOwnProperty('subject')) {
     delete this.subject;
   }
-  if (student.hasOwnProperty('marks')) {
+  if (this.student.hasOwnProperty('marks')) {
     delete this.marks;
   }
   this.excluded = reason;
 }
+
+student2.exclude('low grades')
+console.log(student2)
+
+/*
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
+*/
